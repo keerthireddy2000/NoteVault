f707c29447212187e6e7d3f4651d6b999f76babc
keerthireddytummalapelly <t.y.reddy19@gmail.com>
2024-11-28 00:25:17 -0500

    Home styling changes

--- NoteVault-main/frontend/src/pages/home.js
+++ NoteVault-main/frontend/src/pages/home.js
@@ -1,9 +1,11 @@
+
 import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { FaSearch, FaPlusCircle, FaThumbtack, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { apiCallWithToken } from '../api';
 import { toast , ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { IoClose } from "react-icons/io5";
 
 const Home = () => {
   const [notes, setNotes] = useState([]);
@@ -21,26 +23,29 @@ const Home = () => {
   const navigate = useNavigate();
   const [categoriesDict, setCategoriesDict] = useState([{}]);
 
-  const categoriesToShow = 3;
+  const categoriesToShow = 8;
 
   useEffect(() => {
     const fetchCategories = async () => {
       try {
-        const categoriesResponse = await apiCallWithToken('http://localhost:8000/categories/');
+        const categoriesResponse = await apiCallWithToken(
+          "http://localhost:8000/categories/"
+        );
         const categoriesData = await categoriesResponse.json();
-        setCategories([{ _id: 'all', title: 'All' }, ...categoriesData]);
-        setCategoriesDict(categoriesData.reduce((acc, item) => {
-          acc[item.id] = item.title;
-          return acc;
-        }, {}));
+        setCategories([{ _id: "all", title: "All" }, ...categoriesData]);
+        setCategoriesDict(
+          categoriesData.reduce((acc, item) => {
+            acc[item.id] = item.title;
+            return acc;
+          }, {})
+        );
       } catch (error) {
-        console.error('Error fetching categories:', error);
+        console.error("Error fetching categories:", error);
       }
     };
 
     fetchCategories();
   }, []);
-
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -59,7 +64,7 @@ const Home = () => {
         const sortedNotes = [...sortedPinnedNotes, ...sortedUnpinnedNotes];
         setNotes(sortedNotes);
       } catch (error) {
-        console.error('Error fetching notes:', error);
+        console.error("Error fetching notes:", error);
       }
     };
   
@@ -198,7 +203,6 @@ const Home = () => {
     setOpenDropdown(openDropdown === categoryId ? null : categoryId);
   };
 
-
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
@@ -238,188 +242,238 @@ const Home = () => {
 
   return (
     <div className="min-h-screen bg-white text-white flex flex-col items-center p-4">
-      <div className="flex items-center w-full max-w-5xl mb-4 space-x-4">
-        <div className="flex items-center bg-white border border-black rounded-full px-4 py-2 w-3/5">
-          <FaSearch className="text-gray-500 mr-2" />
-          <input
-            type="text"
-            placeholder="Search a Note"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            className="bg-transparent focus:outline-none text-black w-full"
-          />
-        </div>
-        <div className="flex items-center space-x-2 text-black ">
+      {/* search and filter section of the page */}
+      <div className="flex flex-col w-full mb-8 space-y-4">
+        {/* Search Bar */}
+        <div className="flex items-center space-x-4 mb-4 w-2/3">
+          {/* Search Bar */}
+          <div className="flex items-center bg-white border border-black rounded-full px-4 py-2 flex-grow">
+            <input
+              type="text"
+              placeholder="Search a note by title or category"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              className="bg-transparent focus:outline-none text-black w-full"
+            />
+            <FaSearch className="text-black hover:text-gray-600 mr-2" />
+          </div>
 
+          {/* Button */}
+          <button
+            onClick={() => navigate("/create-note")}
+            className="bg-black border-black border text-white py-2 px-4 rounded-full flex items-center"
+          >
+            <FaPlusCircle className="mr-2" /> Add Note
+          </button>
+        </div>
+
+        {/* Categories */}
+        <div className="flex flex-wrap items-center space-x-2 text-black">
           {visibleCategoryStartIndex > 0 && (
-            <button className="!bg-white border-[2px] py-2 px-4 rounded-full" onClick={handlePrevCategory}>
+            <button
+              className="!bg-white border-[2px] py-2 px-4 rounded-full"
+              onClick={handlePrevCategory}
+            >
               <FaArrowLeft className="text-black" />
             </button>
           )}
           {categories
-            .slice(visibleCategoryStartIndex, visibleCategoryStartIndex + categoriesToShow)
+            .slice(
+              visibleCategoryStartIndex,
+              visibleCategoryStartIndex + categoriesToShow
+            )
             .map((category) => (
-              <div key={category.id} className="relative group flex">
-                <button
-                  onClick={() => setSelectedCategoryId(category.id ? category.id : 'all')}
-                  className={`flex items-center py-2 px-4 rounded-full ${selectedCategoryId === category.id || (category.id === undefined && selectedCategoryId === 'all') ? 'bg-black text-white' : 'bg-white border border-black'
+              <div key={category.id} className="relative group flex w-[150px]">
+  <button
+    onClick={() =>
+      setSelectedCategoryId(category.id ? category.id : "all")
+    }
+    className={`flex items-center justify-between p-2 px-4 rounded-full w-full ${
+      selectedCategoryId === category.id ||
+      (category.id === undefined && selectedCategoryId === "all")
+        ? "bg-black text-white p-[0.6rem]"
+        : "bg-white border border-black"
+    }`}
+  >
+    <span className="truncate max-w-[100px]">{category.title}</span>
+    {category.id && category.id !== "all" && (
+      <div className="ml-2">
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+            handleDropdownToggle(category.id);
+          }}
+          className="text-black text-xl p-0 bg-gray-200 rounded-full w-4"
+        >
+          &#8942;
+        </button>
+      </div>
+    )}
+  </button>
+  {openDropdown === category.id && (
+    <div
+      ref={dropdownRef}
+      className="absolute top-8 right-6 mt-0 bg-white border rounded-md shadow-lg w-28 z-10"
+    >
+      <ul className="space-y-1">
+        <li>
+          <button
+            onClick={() => openEditModal(category.id, category.title)}
+            className="block py-2 px-10 text-black hover:bg-gray-600"
+          >
+            Edit
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={() => openDeleteModal(category)}
+            className="block py-2 px-8 text-black hover:bg-gray-600 rounded-sm"
+          >
+            Delete
+          </button>
+        </li>
+      </ul>
+    </div>
+  )}
+</div>
 
-                    }`}
-                >
-                  {category.title}
-                  {category.id && category.id !== 'all' && (
-                    <div className="ml-2">
-                      <button
-                        onClick={(e) => {
-                          e.stopPropagation();
-                          handleDropdownToggle(category.id);
-                        }}
-                        className="text-black text-xl p-0 bg-gray-200 rounded-full w-4"
-                      >
-                        &#8942;
-                      </button>
-                    </div>
-                  )}
-                </button>
-                {openDropdown === category.id && (
-                  <div
-                    ref={dropdownRef}
-                    className="absolute top-8 right-6 mt-0 bg-white border rounded-md shadow-lg w-28 z-10"
-                  >
-                    <div className="absolute top-0 right-0 p-1">
-                      <button
-                        onClick={(e) => {
-                          e.stopPropagation();
-                        }}
-                        className="text-white text-xl bg-transparent p-0"
-                      >
-                        &#8942;
-                      </button>
-                    </div>
-                    <ul className="space-y-1">
-                      <li>
-                        <button
-                          onClick={() => openEditModal(category.id, category.title)}
-                          className="block py-2 px-10 text-gray-800 hover:bg-gray-100"
-                        >
-                          Edit
-                        </button>
-                      </li>
-                      <li>
-                        <button
-                          onClick={() => openDeleteModal(category)}
-                          className="block py-2 px-8 text-red-600 hover:bg-gray-100 rounded-sm"
-                        >
-                          Delete
-                        </button>
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </div>
             ))}
 
           {visibleCategoryStartIndex + categoriesToShow < categories.length && (
-            <button className="!bg-white  border-[2px] py-2 px-4 rounded-full" onClick={handleNextCategory}>
+            <button
+              className="!bg-white border-[2px] py-[0.75rem] px-4 rounded-md border-black"
+              onClick={handleNextCategory}
+            >
               <FaArrowRight className="text-black" />
             </button>
           )}
-          <button id="addcat"
-            className="bg-white py-2 px-4 rounded-full"
+
+          <button
+            className="bg-black text-white px-4 p-[0.6rem] rounded-full flex items-center"
             onClick={() => setIsModalOpen(true)}
           >
-            <FaPlusCircle className="text-black text-3xl" />
+            <FaPlusCircle className="mr-2" /> Add Category
           </button>
         </div>
       </div>
 
-      <div className="w-full max-w-5xl grid grid-cols-4 gap-4">
+      <div className="w-full overflow-y-auto grid grid-cols-4 gap-4">
         {filteredNotes.length > 0 ? (
           filteredNotes.map((note) => (
             <div
               key={note._id}
-              className="bg-gray-100 p-4 rounded-lg border border-gray-400 shadow-lg relative cursor-pointer"
+              className="bg-gray-100 p-4 rounded-md border border-gray-400 shadow-lg relative cursor-pointer h-32"
               onClick={() => handleNoteClick(note.id)}
             >
               <h3 className="text-xl font-bold text-black">{note.title}</h3>
-              <p className="text-gray-700">{note.content}</p>
-              <span className="text-sm text-gray-700">#{categoriesDict[note.category]}</span>
+              <p className="text-gray-700 truncate">{note.content}</p>
+              <span className="text-sm text-gray-700">
+                #{categoriesDict[note.category]}
+              </span>
+
+              {/* Pin Icon */}
               <FaThumbtack
-                className={`absolute top-2 right-2 cursor-pointer ${note.pinned ? 'text-red-700' : 'text-gray-400'
-                  }`}
+                className={`absolute top-2 right-2 cursor-pointer ${
+                  note.pinned ? "text-black" : "text-gray-600"
+                }`}
                 onClick={(e) => {
                   e.stopPropagation();
-                  console.log(note.id)
-                  togglePin(note.id)
+                  togglePin(note.id);
                 }}
               />
             </div>
           ))
         ) : (
-          <p className="text-black">No notes available in this category.</p>
+          <p className="text-black text-lg">
+            No notes available in this category!!!
+          </p>
         )}
       </div>
 
+      {isModalOpen && (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80">
+    <div className="bg-white p-6 rounded-lg shadow-lg w-1/3 relative">
+      {/* Close Button */}
       <button
-        onClick={() => navigate('/create-note')}
-        className="fixed bottom-10 right-10 bg-black border-black border text-white py-2 px-4 rounded-full flex items-center"
+        onClick={() => setIsModalOpen(false)}
+        className="absolute top-2 right-2 text-xl text-black hover:text-gray-600 p-2"
+        aria-label="Close"
       >
-        <FaPlusCircle className="mr-2" /> Create a new Note
+        <IoClose />
       </button>
 
-      {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80">
-          <div className="bg-black p-6 rounded-lg shadow-lg w-1/3">
-            <h3 className="text-2xl mb-4">{editingCategoryId ? 'Edit Category' : 'Create New Category'}</h3>
-            <input
-              type="text"
-              className="w-full p-2 mb-4 bg-white border border-gray-600 text-black placeholder-gray-400 rounded"
-              placeholder="Enter category title"
-              value={newCategoryTitle}
-              onChange={(e) => setNewCategoryTitle(e.target.value)}
-            />
-            <div className="flex justify-end space-x-4">
-              <button
-                onClick={() => { handleCancel()}}
-                className="bg-white text-black py-2 px-4 rounded hover:bg-red-600"
-              >
-                Cancel
-              </button>
-              <button
-                onClick={() => { editingCategoryId ? handleSaveCategory(editingCategoryId) : handleSaveCategory(""); }}
-                className="bg-white text-black py-2 px-4 rounded hover:bg-green-700"
+      {/* Modal Content */}
+      <h3 className="text-2xl mb-4 text-black">
+        {editingCategoryId ? "Edit Category" : "Create New Category"}
+      </h3>
+      <input
+        type="text"
+        className="w-full p-2 mb-4 bg-white border border-gray-600 text-black placeholder-gray-400 rounded"
+        placeholder="Enter category title"
+        value={newCategoryTitle}
+        onChange={(e) => setNewCategoryTitle(e.target.value)}
+      />
+      <div className="flex justify-end space-x-4">
+        <button
+          onClick={handleCancel}
+          className="bg-black text-white py-2 px-4 rounded hover:bg-gray-600"
+        >
+          Cancel
+        </button>
+        <button
+          onClick={() =>
+            editingCategoryId
+              ? handleSaveCategory(editingCategoryId)
+              : handleSaveCategory("")
+          }
+          className="bg-black text-white py-2 px-4 rounded hover:bg-gray-600"
+        >
+          {editingCategoryId ? "Update" : "Save"}
+        </button>
+      </div>
+    </div>
+  </div>
+)}
 
-              >
-                {editingCategoryId ? 'Update' : 'Save'}
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
 
 
       {isDeleteModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-black p-6 rounded-lg shadow-lg w-1/3">
-            <h3 className="text-2xl mb-4 text-white">
-              Are you sure you want to delete this category - <span className="font-bold">{categoryToDelete?.title}</span>?
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80">
+          <div className="relative bg-white p-6 rounded-md w-1/3 text-black border border-black">
+            {/* Close Button */}
+            <button
+              onClick={() => setIsDeleteModalOpen(false)}
+              className="absolute top-2 right-2 text-xl text-black hover:text-gray-600 p-2"
+              aria-label="Close"
+            >
+              <IoClose />
+            </button>
+            <h3 className="text-2xl mb-6 text-black">
+              Are you sure you want to delete this category -{" "}
+              <span className="font-bold">{categoryToDelete?.title}</span>?
             </h3>
+
             <div className="flex justify-end space-x-4">
               <button
                 onClick={closeDeleteModal}
-                className="bg-gray-500 hover:bg-gray-600 text-white py-2 px-4 rounded"
+                className="bg-black hover:bg-gray-600 text-white py-2 px-4 rounded-md"
               >
                 No
               </button>
+
               <button
                 onClick={() => handleDeleteCategory(categoryToDelete.id)}
-                className="bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded">
+                className="bg-black hover:bg-gray-600 text-white py-2 px-4 rounded-md"
+              >
                 Yes
               </button>
             </div>
           </div>
         </div>
       )}
+
+
+      
       <ToastContainer position="top-center" autoClose={3000} hideProgressBar />
     </div>
   );
